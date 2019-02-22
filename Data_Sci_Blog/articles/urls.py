899d36cbd71496e438993bd5a672b3c325886679
dapierrot <dapierrot21@gmail.com>
2019-02-21 23:18:18 -0500

    Created url parameters to capture the article slug, as well as created a namespace to let django know which url to grap. Added template tags to output this data.

--- Data_Sci_Blog/articles/urls.py
+++ Data_Sci_Blog/articles/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 # from "." the dot points to your current directory
 from . import views
 
+# Namespace so django knows which url to grab.
+app_name = 'articles'
+
 urlpatterns = [
     # Path to the home page. Will look for just .com
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
+    # named capturing group
+    path('<slug>/', views.article_details, name="detail"),
 ]
--- Data_Sci_Blog/articles/views.py
+++ Data_Sci_Blog/articles/views.py
@@ -1,9 +1,15 @@
 # Be sure to add your apps into the settings.py file, Look for INSTALLED_APPS = [].
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 # Controls what is happening when articles page is visted. 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, "articles/article_list.html", {'articles': articles})
+
+# Captures the slug of each article to recieve a template from the database.
+def article_details(request, slug):
+    return HttpResponse(slug)
+
